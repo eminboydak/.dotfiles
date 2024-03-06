eeb5659b8b4a59c1d5bee588a94ede58d3b8e979
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-03-06 22:31:05 +0300

    scripts: Removed capture.sh

--- scripts/scripts/capture.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-selection=$(slop -k)
-menyoki -q cap --root --size "$selection" png save - | rpaste - 2>/dev/null | xclip -selection clipboard
-notify-send "copied to clipboard!" -a "screenshot" -t 1000
