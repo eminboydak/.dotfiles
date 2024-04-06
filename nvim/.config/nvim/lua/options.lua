ee2ee476ce45b7d409f66f828499784a8fa0862c
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-04-07 01:22:14 +0300

    nvim: Edited options.lua

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -7,7 +7,7 @@
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -31,7 +31,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = 'auto'
 
 -- Decrease update time
 vim.opt.updatetime = 250
@@ -60,3 +60,12 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 
 -- vim: ts=2 sts=2 sw=2 et
+
+-- Make indenting smart
+vim.opt.smartindent = true
+
+-- The number of spaces inserted for each indentation
+vim.opt.shiftwidth = 4
+
+-- Insert 4 spaces for a tab
+vim.opt.tabstop = 4
