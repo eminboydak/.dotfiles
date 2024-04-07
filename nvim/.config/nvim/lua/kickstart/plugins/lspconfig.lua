5039be018f36602d826d844568b576bf8f29cd46
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-04-08 02:04:41 +0300

    nvim: Edited lspconfig.lua

--- nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -135,19 +135,16 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
+        astro = {},
+        bashls = {},
+        clangd = {},
+        cmake = {},
+        cssls = {},
+        docker_compose_language_service = {},
+        dockerls = {},
+        html = {},
+        jsonls = {},
+        ltex = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -162,6 +159,21 @@ return {
             },
           },
         },
+        marksman = {},
+        mdx_analyzer = {},
+        matlab_ls = {},
+        pylsp = {},
+        rust_analyzer = {},
+        tailwindcss = {},
+        yamlls = {},
+        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        --
+        -- Some languages (like typescript) have entire language plugins that can be useful:
+        --    https://github.com/pmizio/typescript-tools.nvim
+        --
+        -- But for many setups, the LSP (`tsserver`) will work just fine
+        -- tsserver = {},
+        --
       }
 
       -- Ensure the servers and tools above are installed
